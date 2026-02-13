52a5e862b6a9004dd0d518d3390874931a62fbf4
mainataetae <maina1216@icloud.com>
2026-02-13 05:47:31 +0000

    型関連のs学習用のコードを書いた

---
+++ type_int.php
@@ -0,0 +1,42 @@
+<?php
+
+$i = 189;
+var_dump($i);
+
+var_dump(PHP_INT_MAX);
+var_dump(PHP_INT_MIN);
+
+$j = PHP_INT_MAX + 1;
+var_dump($j);
+
+$k = PHP_INT_MIN -1;
+var_dump($k);
+
+$f = 3.14;
+var_dump($f);
+
+$s = "hello";
+var_dump($s);
+
+echo $s[1], "\n";
+
+$a = "あいうえお";
+var_dump($a);
+
+echo $a[3], "\n";
+
+$d = "num \n";
+$sd ='num \n';
+var_dump($d);
+var_dump($sd);
+
+$b = true;
+$z = false;
+var_dump($b);
+var_dump($z);
+
+$o = TRUE;
+var_dump($o);
+
+$l = null;
+var_dump($l);
