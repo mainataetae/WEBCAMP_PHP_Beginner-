54ae28807f99c100d76ffb1812a3031eb5908720
mainataetae <maina1216@icloud.com>
2026-02-13 07:47:07 +0000

    compare_float.phpを書いた

---
+++ compare_float.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "普通に比較 \n";
+$f = 0.3;
+var_dump($f === 0.3);
+
+echo "\n";
+echo "0.1+0.2と0.3を比較\n";
+$f = 0.2 + 0.1;
+var_dump($f === 0.3);
+
+echo "\n";
+echo"0.1+0.2と0.3をprintfで表示 \n";
+printf("0.1+0.2: %f \n", $f);
+printf("0.3: %f \n",0.3);
+
+echo "\n";
+echo"0.1+0.2と0.3を高精度のrintfで表示 \n";
+printf("0.1+0.2: %.32f \n",$f);
+printf("0.3 : %.32f \n",0.3);
+
+echo "\n";
+echo "0.1+0.2 と 0.3 の現実的な比較\n";
+define('FLOAT_PRECISION', 0.000001); 
+var_dump( abs($f - 0.3) < FLOAT_PRECISION );
