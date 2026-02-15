939531a5cd46086ca86c96316dc9d7c027cbe0f3
mainataetae <maina1216@icloud.com>
2026-02-15 11:19:04 +0000

    math.phpを書いた

--- explode_implode.php
+++ explode_implode.php
@@ -12,3 +12,25 @@ $s = implode(";", $e);
 echo "\n implodeで繋げた文字列 \n";
 var_dump($s);
 
+$c =[
+    "Red",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Black",
+    ];
+foreach($c as $k){
+    echo "{$k} \n";
+}
+
+$h = [
+    "Red" => "#ff0000",
+    "Yellow" => "#ffff00",
+    "Green" => "#00ff00",
+    "Blue" => "#0000ff",
+    "Black" => "#000000",
+];
+
+foreach($h as $v => $l){
+    echo "{$v} : {$l} \n";
+}
